835c1284298c69cf47ae82ceed3ee025f5dd721e
Adrien Meynard <38944960+AdMeynard@users.noreply.github.com>
2018-10-24 16:35:48 +0200

    amari

--- BSS/amari.m
+++ BSS/amari.m
@@ -4,7 +4,7 @@ function indexdB = amari(Best, A)
 G = Best*A; % should be close to identity
 N = size(G,1);
 
-index = (1/( 2*N*(N-1) ))*( sum( sum(abs(G).^2,2)./max(abs(G).^2,[],2) ) + ...
-    sum( sum(abs(G).^2,1)./max(abs(G).^2,[],1) ) - 2*N );
+index = (1/( 2*N*(N-1) ))*( sum( sum(abs(G),2)./max(abs(G),[],2) ) + ...
+    sum( sum(abs(G),1)./max(abs(G),[],1) ) - 2*N );
 
 indexdB = 10*log10(index);
